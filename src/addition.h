f41527016b1aa07f5c9da5926a21a472b15d8d1c
Pablo Joaquim <pablojoaquim@gmail.com>
2025-11-14 11:36:06 -0300

    Several comments has been added to the h file to improve the template

--- src/addition.h
+++ src/addition.h
@@ -1,6 +1,62 @@
 #ifndef ADDITION_H
 #define ADDITION_H
 
+/*===========================================================================*/
+/**
+ * @file addition.h
+ *
+ *  Describe the purpose of this software module in here
+ *
+ *------------------------------------------------------------------------------
+ *
+ * @section DESC DESCRIPTION:
+ *
+ * @todo Add full description here
+ *
+ * @section ABBR ABBREVIATIONS:
+ *   - @todo List any abbreviations, precede each with a dash ('-').
+ *
+ * @section TRACE TRACEABILITY INFO:
+ *   - Design Document(s):
+ *     - @todo Update list of design document(s).
+ *
+ *   - Requirements Document(s):
+ *     - @todo Update list of requirements document(s)
+ *
+ *   - Applicable Standards (in order of precedence: highest first):
+ *     - @todo Update list of other applicable standards
+ *
+ */
+/*==========================================================================*/
+
+/*===========================================================================*
+ * Header Files
+ *===========================================================================*/
+
+#   ifdef __cplusplus
+extern "C"
+{ /* ! Inclusion of header files should NOT be inside the extern "C" block */
+#   endif /* __cplusplus */
+/*===========================================================================*
+ * Exported Preprocessor #define Constants
+ *===========================================================================*/
+
+/*===========================================================================*
+ * Exported Preprocessor #define MACROS
+ *===========================================================================*/
+
+/*===========================================================================*
+ * Exported Type Declarations
+ *===========================================================================*/
+
+/*===========================================================================*
+ * Exported Function Prototypes
+ *===========================================================================*/
 int addition(int a, int b);
 
-#endif
+#   ifdef __cplusplus
+}         /* extern "C" */
+#   endif /* __cplusplus */
+/*===========================================================================*/
+/*===========================================================================*/
+#endif /* ADDITION_H */
